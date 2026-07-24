38ce26b7e7592ba2089991eaded55db87de8ee95
Cristian <crial0810@gmail.com>
2026-07-23 19:25:20 -0500

    Feat: Soporte Híbrido Presencial para iPhone (URL Deep Link Tag) y Android (Web NFC)

--- Main.js
+++ Main.js
@@ -1,5 +1,7 @@
-function doGet() {
-  return HtmlService.createTemplateFromFile('Index')
+function doGet(e) {
+  const template = HtmlService.createTemplateFromFile('Index');
+  template.urlParams = (e && e.parameter) ? JSON.stringify(e.parameter) : '{}';
+  return template
     .evaluate()
     .setTitle('Workerview - Dashboard')
     .addMetaTag('viewport', 'width=device-width, initial-scale=1, maximum-scale=1')
